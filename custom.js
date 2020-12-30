36635b0a85d9fa32a937342b80611a8f6c3f4359
vikram700 <vs4110690@gmail.com>
2020-12-30 14:44:44 +0530

    ad

--- custom.js
+++ custom.js
@@ -1,44 +1,159 @@
 $(function () {
-	$('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+    $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
 });
 $(document).ready(function () {
-	$('[data-bs-toggle="popover"]').popover({
-		trigger: 'hover',
+    $('[data-bs-toggle="popover"]').popover({
+        trigger: 'hover',
 
-	});
+    });
 });
 
 // parent slider code
 
 
- var $owl = $('#carousel1');
-    $owl.children().each(function (index) {
-        $(this).attr('data-position', index);
+var $owl = $('#carousel1');
+$owl.children().each(function (index) {
+    $(this).attr('data-position', index);
 
-    });
-    $('#carousel1').owlCarousel({
-        loop: true,
-        center: true,
-        margin: 0,
-        responsiveClass: true,
-        nav: false,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        autoplayHoverPause: true,
+});
+$('#carousel1').owlCarousel({
+    loop: true,
+    center: true,
+    margin: 0,
+    responsiveClass: true,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
 
-        responsive: {
-            0: {
-                items: 1
-            },
-            680: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            }
+    responsive: {
+        0: {
+            items: 1
+        },
+        680: {
+            items: 2
+        },
+        1000: {
+            items: 3
         }
-    })
-    $(document).on('click', '.owl-item>div', function () {
-        var $speed = 1000;
-        $owl.trigger('to.owl.carousel', [$(this).data('position'), $speed]);
-    });
+    }
+})
+$(document).on('click', '.owl-item>div', function () {
+    var $speed = 1000;
+    $owl.trigger('to.owl.carousel', [$(this).data('position'), $speed]);
+});
+
+
+
+
+
+
+var $owl = $('.photo-slide-img');
+$owl.children().each(function (index) {
+    $(this).attr('data-position', index);
+
+});
+$('.photo-slide-img').owlCarousel({
+    loop: true,
+    center: true,
+    margin: 0,
+    responsiveClass: true,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+
+    responsive: {
+        0: {
+            items: 1
+        },
+        680: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+var $owl = $('.news-slide');
+$owl.children().each(function (index) {
+    $(this).attr('data-position', index);
+
+});
+$('.news-slide').owlCarousel({
+    loop: true,
+    center: true,
+    margin: 0,
+    responsiveClass: true,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+
+    responsive: {
+        0: {
+            items: 1
+        },
+        680: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+var $owl = $('.result');
+$owl.children().each(function (index) {
+    $(this).attr('data-position', index);
+
+});
+$('.result').owlCarousel({
+    loop: true,
+    center: true,
+    margin: 0,
+    responsiveClass: true,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+
+    responsive: {
+        0: {
+            items: 1
+        },
+        680: {
+            items: 2
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+var $owl = $('.slider-home');
+$owl.children().each(function (index) {
+    $(this).attr('data-position', index);
+
+});
+$('.slider-home').owlCarousel({
+    loop: true,
+    center: true,
+    margin: 0,
+    responsiveClass: true,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+
+    responsive: {
+        0: {
+            items: 1
+        },
+        680: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+//   console.log($owl)
