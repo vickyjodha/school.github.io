0c879bae96159ec04acddde972a2498b2c18a1c8
vikram700 <vs4110690@gmail.com>
2020-12-31 17:15:31 +0530

    fian

--- custom.js
+++ custom.js
@@ -156,4 +156,61 @@ $('.slider-home').owlCarousel({
         }
     }
 })
-//   console.log($owl)
+//   main slide use 
+
+var $owl = $('.slide-home-page');
+$owl.children().each(function (index) {
+    $(this).attr('data-position', index);
+
+});
+$('.slide-home-page').owlCarousel({
+    loop: true,
+    center: true,
+    margin: 0,
+    responsiveClass: true,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+
+    responsive: {
+        0: {
+            items: 1
+        },
+        680: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+
+
+var $owl = $('.teacher-slide');
+$owl.children().each(function (index) {
+    $(this).attr('data-position', index);
+
+});
+$('.teacher-slide').owlCarousel({
+    loop: true,
+    center: true,
+    margin: 0,
+    responsiveClass: true,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+
+    responsive: {
+        0: {
+            items: 1
+        },
+        680: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
