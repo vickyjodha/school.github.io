a6f6d64dfce22db073f7125cc6f2a270d8f254d3
vikram700 <vs4110690@gmail.com>
2021-01-15 17:55:02 +0530

    brocher

---
+++ brocher/coum.js
@@ -0,0 +1,7 @@
+var searchPic ="../assets/images/logo-1.png"
+//  var dd=$("#schoolImg").attr("src", searchPic);
+//  console.log(dd)
+var dds=$("#schoolImg").src="../assets/images/logo-1.png";
+ console.log(dds)
+//  document.getElementById("schoolImg").src = "../assets/images/logo-1.png"; 
+// document.getElementById('schoolImg').src="../assets/images/logo-1.png";
